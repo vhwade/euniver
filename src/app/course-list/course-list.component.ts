cff97f90d227741329c0f3dacf191d1203b724b3
vhwade <volodymyr.hikovatyi@hneu.net>
2017-10-23 02:26:23 +0300

    Data Binding

--- src/app/course-list/course-list.component.ts
+++ src/app/course-list/course-list.component.ts
@@ -9,15 +9,31 @@ export class CourseListComponent {
 
 	courses: Model.Course[];
 
+	activeOnly: boolean;
+
 	constructor() {
+
+		this.activeOnly = true;
+
 		this.courses = [
 			{
 				id: 1,
 				name: 'Розробка Web-додатків',
-				summary: 'Дисципліна спрямована на формування у студентів теоретичних знань, практичних вмінь та навичок щодо розробки Web-додатків.'
+				summary: 'Дисципліна спрямована на формування у студентів теоретичних знань, практичних вмінь та навичок щодо розробки Web-додатків.',
+				isActive: true
 			},
-			{ id: 2, name: 'Цифрова фотографія' },
-			{ id: 3, name: 'Основи програмування' }
+			{ id: 2, name: 'Цифрова фотографія', isActive: true },
+			{ id: 3, name: 'Основи програмування', isActive: false }
 		];
+	}
+
+	filterCoursesByActiveStatus(event) {
+		this.activeOnly = !this.activeOnly;
+		console.log('Стан змінено');
+		console.log(event);
+	}
+
+	writeCourseInfo(course: Model.Course) {
+		console.log(course);
 	}
 }
--- src/app/model/model.ts
+++ src/app/model/model.ts
@@ -24,6 +24,7 @@ namespace Model {
 	export class Course {
 		public id: number;
 		public name: string;
+		public isActive: boolean;
 		public summary ?: string;
 	}
 
